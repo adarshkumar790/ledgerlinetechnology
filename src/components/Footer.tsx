6cacf147e743e76dd3937286aa1d5dc2120feb77
AdarhKumar <adarshnit420@gmail.com>
2024-12-30 12:53:59 +0530

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ import { ImInstagram } from "react-icons/im";
 const Footer: React.FC = () => {
   return (
     <footer className="relative bg-[#393D42] text-gray-300 py-8 w-full">
-      <p className="absolute -top-4 sm:-top-6 left-1/2 transform -translate-x-1/2 text-white font-bold text-2xl sm:text-4xl md:text-4xl text-center whitespace-nowrap">
-        Innovation Beyond Boundaries
+      <p className="absolute -top-4 sm:-top-5 left-1/2  transform -translate-x-1/2 text-white font-bold text-2xl sm:text-4xl md:text-4xl text-center whitespace-nowrap">
+        INNOVATION BEYOND BOUNDARIES
       </p>
 
       <div className="max-w-[900px] mx-auto px-4 md:flex md:justify-between md:items-start mt-20">
@@ -59,7 +59,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="mt-8 md:mt-0 md:flex md:space-x-16 pl-9 sm:pl-6">
+        <div className="mt-8 md:mt-0 md:flex md:space-x-14 pl-9 sm:pl-0">
           <div>
             <h3 className="text-white font-bold mb-4">Links</h3>
             <ul className="space-y-2">
@@ -91,8 +91,8 @@ const Footer: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-white font-bold mb-2 mt-4">Services</h3>
-            <ul className="space-y-2">
+            <h3 className="text-white font-bold mb-2 sm:mb-2 mt-4 md:mt-0 ">Services</h3>
+            <ul className="space-y-2 mt-4 sm:mt-4">
               <li><a href="#" className="text-white">Blockchain</a></li>
               <li><a href="#" className="text-white">Metaverse</a></li>
               <li><a href="#" className="text-white">Solution</a></li>
@@ -101,11 +101,11 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="mt-8 md:mt-0 md:flex md:space-x-16 pl-9 sm:pl-6">
+        <div className="mt-8 md:mt-0 space-x-0  md:space-x-0 pl-9 sm:pl-0">
           <h3 className="text-white font-semibold mb-4">Contact</h3>
           <ul className="space-y-4">
             {/* Address */}
-            <li className="flex items-start space-x-4">
+            <li className="flex items-start space-x-2">
               <Image
                 src="/lo.png" 
                 alt="Address Icon"
