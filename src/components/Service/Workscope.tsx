330fd5eeb5f878e9b8de0243eb43b9f388346e7e
AdarhKumar <adarshnit420@gmail.com>
2024-12-31 10:49:24 +0530

    first commit

--- src/components/Service/Workscope.tsx
+++ src/components/Service/Workscope.tsx
@@ -4,17 +4,19 @@ import { FC } from "react";
 const ScopeWork: FC = () => {
   return (
     <div className="bg-black min-h-screen flex items-center justify-center relative">
+      {/* White Rectangle for Scope Of Work (Mobile Only) */}
+      <div className="absolute md:top-52 top-32 bg-white w-full md:h-28 h-12 "></div>
 
-    
-      <div className="absolute top-52 bg-white w-full h-28 overflow-hidden"></div>
-      
-      
-      <div className="max-w-6xl mx-auto flex flex-col items-center md:mt-0 mt-24 ">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
-          
-          
-          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 -top-28 p-8 w-64 h-80 shadow-lg flex flex-col items-center overflow-hidden relative">
-            <div className="w-full h-full absolute  left-0 bg-transparent"></div>
+      {/* White Rectangle for Our Solution (Mobile Only) */}
+      <div className="absolute top-[31rem] bg-white w-full md:h-28 h-12 md:hidden"></div>
+
+      {/* White Rectangle for The Platform (Mobile Only) */}
+      <div className="absolute top-[55rem] bg-white w-full md:h-28 h-12 md:hidden"></div>
+
+      <div className="max-w-6xl mx-auto flex flex-col items-center md:mt-0 md:mt-24 mt-0">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-8 text-center">
+          {/* Scope Of Work Box */}
+          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 p-8 w-64 h-80 shadow-lg flex flex-col items-center overflow-hidden relative">
             <div className="text-white text-4xl mb-4 p-5 rounded-full bg-white">
               <Image src="/job1.png" alt="Briefcase" width={40} height={40} />
             </div>
@@ -24,9 +26,8 @@ const ScopeWork: FC = () => {
             </p>
           </div>
 
-          
-          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 p-8 w-64 h-80 ml-4 -mt-16 shadow-lg flex flex-col items-center overflow-hidden relative">
-            
+          {/* Our Solution Box */}
+          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 p-8 w-64 h-80 shadow-lg flex flex-col items-center overflow-hidden relative">
             <div className="text-white text-4xl mb-4 p-5 rounded-full bg-white">
               <Image src="/job2.png" alt="Briefcase" width={40} height={40} />
             </div>
@@ -36,9 +37,8 @@ const ScopeWork: FC = () => {
             </p>
           </div>
 
-          
-          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 p-8 w-64 h-80 ml-6  shadow-lg flex flex-col items-center overflow-hidden relative">
-            <div className="w-full h-full absolute top-0 left-0 bg-transparent"></div>
+          {/* The Platform Box */}
+          <div className="bg-[#393D42] hover:border-b-blue-800 border border-white border-b-8 p-8 w-64 h-80 shadow-lg flex flex-col items-center overflow-hidden relative">
             <div className="text-white text-4xl mb-4 p-5 rounded-full bg-white">
               <Image src="/job3.png" alt="Briefcase" width={40} height={40} />
             </div>
