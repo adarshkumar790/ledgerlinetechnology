f8e5a83d61cde232971ee529368ef270f0be972c
AdarhKumar <adarshnit420@gmail.com>
2024-12-31 15:00:07 +0530

    first commit

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,33 @@
 import ContactForm from '@/components/Contact/Contactform';
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const ContactUs: React.FC = () => {
   return (
     <>
-    <Navbar/>
-    <div className="relative w-full h-[445px] bg-cover bg-center" style={{ backgroundImage: 'url("/contact.png")' }}>
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-blue-50/20 to-black/40"></div>
-      <div className="absolute inset-0">
-        <div className="absolute w-[300px] h-[300px] bg-blue-400 opacity-30 rounded-full blur-[150px] top-10 left-10"></div>
-        <div className="absolute w-[200px] h-[200px] bg-purple-400 opacity-30 rounded-full blur-[120px] bottom-10 right-10"></div>
+  
+      <Navbar />
+      <div 
+        className="relative md:w-full h-[300px] sm:h-[300px] bg-cover bg-center" 
+        style={{ backgroundImage: 'url("/contact.png")' }}
+      >
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-blue-50/20 to-black/40"></div>
+        <div className="absolute inset-0">
+          <div 
+            className="absolute w-[300px] h-[300px] sm:w-[150px] sm:h-[150px] bg-blue-400 opacity-30 rounded-full blur-[150px] sm:blur-[75px] top-10 left-10"
+          ></div>
+          <div 
+            className="absolute w-[200px] h-[200px] sm:w-[100px] sm:h-[100px] bg-purple-400 opacity-30 rounded-full blur-[120px] sm:blur-[60px] bottom-10 right-10"
+          ></div>
+        </div>
       </div>
-    </div>
-    <ContactForm/>
-    </>
+      <div className="px-4 sm:px-2 mb-1">
+        <ContactForm />
+      </div>
+      
+      
+      </>
   );
 };
 
--- src/components/Contact/Contactform.tsx
+++ src/components/Contact/Contactform.tsx
@@ -37,55 +37,55 @@ const ContactForm: React.FC = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center mt-10">
-        <h3 className="text-[#FFFFFF] font-semibold text-4xl mb-2">GET IN TOUCH</h3>
+      <div className="flex flex-col items-center justify-center mt-8 mb-12">
+        <h3 className="text-[#FFFFFF] font-semibold text-4xl ">GET IN TOUCH</h3>
         <h3 className="text-[#FFFFFF] text-xl">Get in touch with us for any inquiries or support. We’re here to assist you promptly.</h3>
       </div>
 
       <div className="relative w-full min-h-screen flex items-center">
         
-        <div className="absolute inset-0 bg-[#393D42] text-white p-8 flex flex-col justify-center mt-40">
-          <div className="ml-20">
-            <p className="mb-6 text-2xl">
+        <div className="absolute inset-0 bg-[#393D42] text-white p-8 flex flex-col justify-center mt-60">
+          <div className="ml-8 md:mt-0 mt-24 ">
+            <p className="mb-6 md:text-2xl text-sm">
               We’d be delighted to connect with you! <br /> Complete the form, and we’ll get back <br /> to you at the earliest opportunity.
             </p>
-            <div className="space-y-4">
+            <div className="space-y-4 ">
               <div className="flex items-center space-x-3">
                 <Image src="/con1.png" alt="Call" width={20} height={20} />
-                <span>+91 12345 67890</span>
+                <span className='text-xl'>+91 12345 67890</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Image src="/con2.png" alt="Email" width={20} height={20} />
-                <span>contact@ledgerline.com</span>
+                <span className='text-xl'>contact@ledgerline.com</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Image src="/con3.png" alt="Address" width={20} height={20} />
-                <span>901, Silver Samnway, Munjmauda, Akota, Vadodara 390020</span>
+                <span className='text-xl'>901, Silver Samnway, Munjmauda, Akota, Vadodara 390020</span>
               </div>
             </div>
           </div>
         </div>
 
       
-        <div className="relative z-10 bg-white p-6 w-full max-w-md ml-auto mr-32 shadow-lg rounded-2xl overflow-hidden">
+        <div className="relative z-10 bg-white md:-mt-0 -mt-96 md:top-0 top-8 p-6 w-[70%] md:w-[28%] max-w-md ml-auto md:mr-32 mr-16  shadow-lg rounded-2xl overflow-hidden">
           <h2 className="text-xl font-semibold text-[#000000] mb-4">Send us a message</h2>
-          <form ref={form} onSubmit={sendEmail} className="space-y-4">
+          <form ref={form} onSubmit={sendEmail} className="space-y-1">
             <div>
-              <label className="block mb-1 text-[#000000]">First Name</label>
+              <label className="block   text-[#000000]">First Name</label>
               <input
                 type="text"
                 name="first_name"
-                className="w-full text-gray-800 border-0 border-b border-b-gray-700 p-2 focus:outline-none focus:ring-0 focus:border-b-blue-500"
+                className="w-full text-gray-800 mb-1 md:mb-2 border-0 border-b border-b-gray-700  focus:outline-none focus:ring-0 focus:border-b-blue-500"
                 style={{ borderRadius: "0" }}
                 required
               />
             </div>
             <div>
-              <label className="block mb-1 text-[#000000]">Last Name</label>
+              <label className="block  text-[#000000]">Last Name</label>
               <input
                 type="text"
                 name="last_name"
-                className="w-full text-gray-800 border-0 border-b border-b-gray-700 p-2 focus:outline-none focus:ring-0 focus:border-b-blue-500"
+                className="w-full text-gray-800 mb-1 md:mb-2 border-0 border-b border-b-gray-700  focus:outline-none focus:ring-0 focus:border-b-blue-500"
                 style={{ borderRadius: "0" }}
                 required
               />
@@ -95,18 +95,18 @@ const ContactForm: React.FC = () => {
               <input
                 type="email"
                 name="email"
-                className="w-full text-gray-800 border-0 border-b border-b-gray-700 p-2 focus:outline-none focus:ring-0 focus:border-b-blue-500"
+                className="w-full text-gray-800 mb-1 md:mb-2 border-0 border-b border-b-gray-700  focus:outline-none focus:ring-0 focus:border-b-blue-500"
                 style={{ borderRadius: "0" }}
                 required
               />
             </div>
             <div>
-              <label className="block mb-1 text-[#000000]">Your Message</label>
+              <label className="block  text-[#000000]">Your Message</label>
               <textarea
                 name="message"
-                className="w-full text-gray-800 border-0 border-b border-b-gray-700 p-2 focus:outline-none focus:ring-0 focus:border-b-blue-500"
+                className="w-full text-gray-800 mb-1 md:mb-2 border-0 border-b border-b-gray-700  focus:outline-none focus:ring-0 focus:border-b-blue-500"
                 style={{ borderRadius: "0" }}
-                rows={4}
+                rows={1}
                 required
               ></textarea>
             </div>
