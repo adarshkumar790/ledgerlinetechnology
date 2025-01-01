756bce80bb012d46b58f886bd44ebd1200b145ad
AdarhKumar <adarshnit420@gmail.com>
2025-01-01 11:06:58 +0530

    first commit

--- src/components/Blog/Blogcard.tsx
+++ src/components/Blog/Blogcard.tsx
@@ -15,7 +15,7 @@ const BlogCard: React.FC = () => {
             className="object-cover w-full h-auto"
           />
 
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:translate-y-0 lg:top-auto lg:bottom-[-20%] lg:left-1/2 lg:transform lg:-translate-x-1/2 w-48 h-20 mt-28 md:mb-16 sm:w-40 sm:h-32 md:w-36 md:h-28 lg:w-140 lg:h-120 z-10">
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:translate-y-0 lg:top-auto lg:bottom-[-20%] lg:left-1/2 lg:transform lg:-translate-x-1/2 w-48 h-20 mt-24 md:mb-16 sm:w-40 sm:h-32 md:w-36 md:h-28 lg:w-140 lg:h-120 z-10">
             <Image
               src="/blog5.png"
               alt="Handshake"
@@ -27,7 +27,7 @@ const BlogCard: React.FC = () => {
         </div>
 
         {/* Content Section */}
-        <div className="flex flex-col justify-center bg-white md:p-6 pt-16 pl-2 pr-2 pb-8 shadow-lg relative lg:-left-28 lg:-mb-12 lg:z-10 mt-8 lg:w-5/7 text-black h-auto sm:mt-12 lg:mt-48">
+        <div className="flex flex-col justify-center bg-white md:p-6 pt-20 pl-2 pr-2 pb-8 shadow-lg relative lg:-left-28 lg:-mb-12 lg:z-10 mt-8 lg:w-5/7 text-black h-auto sm:mt-12 lg:mt-48">
           <h2 className="font-bold mb-4 text-lg sm:text-xl">
             Unlock Telegram’s Potential: A Guide to Mini Apps
           </h2>
